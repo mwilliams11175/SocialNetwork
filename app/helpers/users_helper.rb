395e166cb137bb510d70dbec24f2c14524498ee8
Michael Williams <mwilliams11175@gmail.com>
2015-11-29 22:03:17 -0600

    added friendship status

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+	def action_buttons(user)
+		case current_user.friendship_status(user) 
+			when "friends"
+				"Remove Friendship"
+			when "pending"
+				"Cancel Request"
+			when "requsted"
+				"Accept or Deny"
+			when "not_friends"
+				"Add Friend"
+		end
+	end
+
 end
