ae2efb42603d45e3ab6a4a28938cd451a826899c
Michael Williams <mwilliams11175@gmail.com>
2015-12-01 22:11:58 -0600

    added user detection

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,16 @@ class UsersController < ApplicationController
   end
 
   def index
-  	@users = User.all
+  	case params[:people] when "friends"
+  		@users = current_user.active_friends
+  	when "requests"
+  		@users = current_user.pending_friend_requests_from.map(&:user)
+  	when "pending"
+  		@users = current_user.pending_friend_requests_to(&:friend)
+  	else
+  		@users = User.where.not(id: current_user.id)
+
+  	end
   end
 
 private
